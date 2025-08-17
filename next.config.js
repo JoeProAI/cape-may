a50c4cff56ceb2e3da9abb367cd8683f4bbdeb3b
Joseph Gaither <joe@joepro.ai>
2025-08-17 08:18:31 -0400

    Remove static export to fix Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
   images: {
     unoptimized: true
-  },
-  distDir: 'out'
+  }
 }
 
 module.exports = nextConfig
